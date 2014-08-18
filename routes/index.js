20153576ba41e8dddf24e7328793e0f5714da885
Rorchach <cookmycode@gmail.com>
2014-08-18 23:44:13 +0800

    add one route

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res) {
   res.render('index', { title: 'Express'});
 });
 
+router.get('/reg', function(req, res) {
+  res.render('reg', {title: '注册'});
+});
+
 module.exports = router;
